1cf3736b33a40cefbc14d0974568c600d74cbbce
Camila Morales <camila.moralesg21@gmail.com>
2023-04-15 21:07:32 -0500

    ADD detalle de una pelicula

--- js/search.js
+++ js/search.js
@@ -1,12 +1,12 @@
 const movies = [
-    { id: 1, name: 'Ballena', year: 2018, category: ['drama', ' terror'], score: '1.0', image: 'img/ballena.png' },
-    { id: 1, name: 'super man', year: 2023, category: ['drama', 'otra'], score: '8.0', image: 'img/ballena.png' },
-    { id: 1, name: 'hormiga', year: 2017, category: ['drama', 'otra'], score: '2.0', image: 'img/ballena.png' },
-    { id: 1, name: 'Ballena', year: 2021, category: ['drama', 'otra'], score: '8.0', image: 'img/ballena.png' },
-    { id: 1, name: 'Ballena', year: 2023, category: ['drama', 'otra'], score: '3.0', image: 'img/ballena.png' },
-    { id: 1, name: 'Ballena', year: 2019, category: ['drama', 'otra'], score: '9.0', image: 'img/ballena.png' },
-    { id: 1, name: 'Ballena', year: 2022, category: ['drama', 'otra'], score: '8.0', image: 'img/ballena.png' },
-    { id: 1, name: 'Ballena', year: 2023, category: ['drama', 'otra'], score: '5.0', image: 'img/ballena.png' }
+    { id: 1, name: 'Ballena', year: 2018, category: ['drama', ' terror'], score: '1.0', duration:'2h 50min', image: 'img/ballena.png', sipnosis: 'Billy Batson and his foster siblings, who transform into superheroes by saying "Shazam!", are forced to get back into action and fight the Daughters of Atlas, who they must stop from using a weapon that could destroy the world.' },
+    { id: 2, name: 'super man', year: 2023, category: ['drama', 'otra'], score: '8.0', duration:'2h 50min', image: 'img/ballena.png', sipnosis: 'Billy Batson and his foster siblings, who transform into superheroes by saying "Shazam!", are forced to get back into action and fight the Daughters of Atlas, who they must stop from using a weapon that could destroy the world.' },
+    { id: 3, name: 'hormiga', year: 2017, category: ['drama', 'otra'], score: '2.0', duration:'2h 50min', image: 'img/ballena.png', sipnosis: 'Billy Batson and his foster siblings, who transform into superheroes by saying "Shazam!", are forced to get back into action and fight the Daughters of Atlas, who they must stop from using a weapon that could destroy the world.' },
+    { id: 4, name: 'Ballena', year: 2021, category: ['drama', 'otra'], score: '8.0', duration:'2h 50min', image: 'img/ballena.png', sipnosis: 'Billy Batson and his foster siblings, who transform into superheroes by saying "Shazam!", are forced to get back into action and fight the Daughters of Atlas, who they must stop from using a weapon that could destroy the world.' },
+    { id: 5, name: 'Ballena', year: 2023, category: ['drama', 'otra'], score: '3.0', duration:'2h 50min', image: 'img/ballena.png', sipnosis: 'Billy Batson and his foster siblings, who transform into superheroes by saying "Shazam!", are forced to get back into action and fight the Daughters of Atlas, who they must stop from using a weapon that could destroy the world.' },
+    { id: 6, name: 'Ballena', year: 2019, category: ['drama', 'otra'], score: '9.0', duration:'2h 50min', image: 'img/ballena.png', sipnosis: 'Billy Batson and his foster siblings, who transform into superheroes by saying "Shazam!", are forced to get back into action and fight the Daughters of Atlas, who they must stop from using a weapon that could destroy the world.' },
+    { id: 7, name: 'Ballena', year: 2022, category: ['drama', 'otra'], score: '8.0', duration:'2h 50min', image: 'img/ballena.png', sipnosis: 'Billy Batson and his foster siblings, who transform into superheroes by saying "Shazam!", are forced to get back into action and fight the Daughters of Atlas, who they must stop from using a weapon that could destroy the world.' },
+    { id: 8, name: 'Ballena', year: 2023, category: ['drama', 'otra'], score: '5.0', duration:'2h 50min', image: 'img/ballena.png', sipnosis: 'Billy Batson and his foster siblings, who transform into superheroes by saying "Shazam!", are forced to get back into action and fight the Daughters of Atlas, who they must stop from using a weapon that could destroy the world.' }
 ]
 
 const boxSearch = document.querySelector('#box-search');
@@ -35,7 +35,7 @@ const filter = () => {
                         <li class="list-group-item">${movie.category}</li>
                     </ul>
                     <div class="card-body">
-                        <a href="#" class="card-link">Ver detalle</a>
+                        <a href="/movie.html" class="card-link">Ver detalle</a>
                     </div>
                 </div>
             `
